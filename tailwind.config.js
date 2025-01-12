80490110e26f0ee68096d486528f9f969abaedb9
0Davgi0 <0Davgi0@0DAVGI0-PC>
2025-01-12 15:45:29 +0100

    Initialize project with Tailwind CSS setup and basic HTML structure

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}", // Dossier contenant tes fichiers HTML et JS
+  ],
+  theme: {
+    extend: {}, // Ajoute ici tes personnalisations (couleurs, polices, etc.)
+  },
+  plugins: [],
+};
